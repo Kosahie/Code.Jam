dbf1fdcbb2896d0f37e66278e3f95d4a3457e27a
jeanine56 <97963882+jeanine56@users.noreply.github.com>
2022-11-19 12:47:20 -0500

    Added story object
    Stores a starting node and a title

---
+++ src/Story.java
@@ -0,0 +1,21 @@
+
+public class Story {
+
+	private Node start;
+	private String title;
+	
+	public Node getStart() {
+		return start;
+	}
+	public void setStart(Node start) {
+		this.start = start;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+}
--- src/main.java
+++ src/main.java
@@ -1,13 +1,22 @@
+import java.util.Scanner; // Import the Scanner class
 
 public class main {
 
-    public static void main(String[] args){
+	public static void main(String[] args) {
 
-        createStory();
-    }
+		createStory();
+		Scanner myObj = new Scanner(System.in); // Create a Scanner object
+		System.out.println("Enter username");
 
-    public void createStory(){
-        System.out.println("Start of create story");
-        Node initNode = new Node();
-    }
+		String userName = myObj.nextLine(); // Read user input
+		System.out.println("Username is: " + userName); // Output user input
+	}
+
+	public static void createStory() {
+		System.out.println("Start of create story");
+		Node initNode = new Node();
+	}
+
+	public 
+	
 }
